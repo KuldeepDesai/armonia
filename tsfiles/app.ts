7d3ce0ad468a8d9b7ac35c2b19fe6f143b8e80e6
KuldeepDesai <me.kuldeep.desai@gmail.com>
2016-04-17 13:51:36 +0530

    Web Hooks Complete!

---
+++ tsfiles/app.ts
@@ -0,0 +1,12 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: "app-title",
+    template: `<div class="jumbotron">
+        <h1>Armonia!</h1>
+        <p>Harmony within our community!</p>
+        <br/>
+    </div>`
+})
+
+export class AppComponent{}
